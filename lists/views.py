07695b2b91286b1a1571a5987e782e2b4560a9b0
thadio <thadio_robledo@hotmail.com>
2026-04-01 21:19:35 -0300

    Escrevendo código da aplicação

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home_page(request):
+    html = "<html><head><title>To-Do lists</title></head><body></body></html>"
+    return HttpResponse(html)
