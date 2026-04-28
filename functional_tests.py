6ebbf4ce7cec3f01ac27b9145ecc1ff05410d4ef
thadio <thadio_robledo@hotmail.com>
2026-04-27 22:15:31 -0300

    Melhorando a Página Inicial

--- functional_tests.py
+++ functional_tests.py
@@ -37,7 +37,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertTrue(
-            any(row.text == '1: Estudar testes funcionais' for row in rows)
+            any(row.text == '1: Estudar testes funcionais' for row in rows),
+                "New to-do item did not appear in table"
         )
         
         # Ainda existe uma caixa de texto convidando para adicionar outro item
